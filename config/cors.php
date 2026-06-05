7541058cf359e4b598947d4c8f69f17ac3abb31a
Daner Escobar <escobardaner48@gmail.com>
2026-06-04 20:10:25 -0400

    fix(backend): add docker host to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 👇 AQUÍ ESTÁ EL ÚNICO CAMBIO (El puerto de tu React)
-    'allowed_origins' => ['http://localhost:5173'],
+   'allowed_origins' => ['http://localhost:5173', 'http://192.168.100.24', 'http://localhost'],
 
     'allowed_origins_patterns' => [],
 
